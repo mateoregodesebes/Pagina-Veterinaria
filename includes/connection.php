5741f9b989105a85a1bbac018ea5a4fff672881c
JosepSoc <jose.socolsky@gmail.com>
2023-12-18 16:36:44 -0300

    added: folder structure, composer pkgmanager, gitignore

---
+++ includes/connection.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
