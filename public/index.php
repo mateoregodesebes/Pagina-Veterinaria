cd4575737c37ca98e406d6a3b3f760fa7942f0d0
mateoregodesebes <mateoregodesebes123@gmail.com>
2023-12-19 01:02:43 -0300

    Started working in navbar. Added things from crud-mascotas branch

--- public/index.php
+++ public/index.php
@@ -5,13 +5,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css\styles.css">
     <title>Página Principal</title>
 </head>
+<?php
+/* TODO: 
+        -   Add the links to their respective pages to each list item from the navbar, default is "#".
+        -   Add another list item for the profile thing. */
+?>
 
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <div class="container">
+        <div class="row">
+            <nav class="nav navbar-expand d-flex justify-content-between">
+                <a class="navbar-brand mx-3" href="#">
+                    <img src="../assets/logo.png" width="50" height="50" alt="Logo">
+                </a>
+
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre Nosotros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Noticias Peludas</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
 
         <footer>
             <div class="row">
