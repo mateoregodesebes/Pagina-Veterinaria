3935e56bef1feb2399c61b491ff8fdcba5cb3348
JosepSoc <jose.socolsky@gmail.com>
2023-12-20 11:14:00 -0300

    added folder structure and footer moved to partials

---
+++ src/partials/footer.php
@@ -0,0 +1,21 @@
+<div class="col-6 footerblock">
+  <address>
+    Dirección: 1234 Calle Prueba<br>
+    <a href="https://www.google.com/maps">Ver en Google Maps</a><br>
+    Teléfono: 341 - 1234567
+  </address>
+</div>
+<div class="col-6 footerblock">
+  <a href="QuienesSomos.php">Quienes somos</a>
+  <p>Nuestras redes sociales:</p>
+  <div class="row">
+    <div class="col-6">
+      <a href="https://www.facebook.com/">Nuestro Facebook</a>
+    </div>
+    <div class="col-6">
+      <a href="https://www.instagram.com/">Nuestro Instagram</a>
+    </div>
+  </div>
+</div>
+<p>Este sitio es realizado para una prueba práctica de la materia Entornos Gráficos de la Universidad Tecnologica
+  Nacional Regional Rosario (UTNFRRO), no representa a ninguna veterinaria ni debe ser tomada como ello.</p>
