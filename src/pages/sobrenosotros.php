dcc1182c1425cb7c08daa66f4bf127bf51d6fa86
MQRLZ <martinez34950@gmail.com>
2023-12-19 21:34:25 -0300

    added sobrenosotros.php with aboutus.css

---
+++ src/pages/sobrenosotros.php
@@ -0,0 +1,33 @@
+<div class="container-fluid">
+    <div class="pag">
+        <div class="row">
+            <h1>Sobre Nosotros</h1>
+            <h2>¿Quienes somos?</h2>
+            <p>Somos una veterinaria que se dedica a la atención de mascotas, con más de 10 años de experiencia en el rubro.</p>
+            <h2>¿Qué hacemos?</h2>
+            <p>Atendemos a mascotas de todo tipo, desde perros y gatos hasta reptiles y aves. Contamos con un equipo de profesionales altamente capacitados para atender a tu mascota.</p>
+            <h2>¿Dónde estamos?</h2>
+            <p>Estamos ubicados en la calle 1234, en la ciudad de Rosario, Santa Fe, Argentina.</p>
+            <h2>¿Cómo contactarnos?</h2>
+            <p>Podés contactarnos por teléfono al 341 - 1234567, o por correo electrónico a
+                <a href="mailto:valenalesa@gmail.com">nuestro mail personal</a>.</p>
+            <h2>¿Cuáles son nuestros horarios?</h2>
+            <p>Nuestros horarios son de lunes a viernes de 8 a 20hs, y los sábados de 8 a 13hs.</p>
+            <h2>¿Qué redes sociales tenemos?</h2>
+            <p>Podés encontrarnos en <a href="https://www.facebook.com/">Facebook</a> e <a href="https://www.instagram.com/">Instagram</a>.</p>
+            <h2>¿Qué servicios ofrecemos?</h2>
+            <p>Ofrecemos servicios de atención veterinaria, peluquería canina y felina, y venta de alimentos y accesorios para mascotas.</p>
+            <h2>¿Qué productos vendemos?</h2>
+            <p>Vendemos alimentos y accesorios para mascotas, como por ejemplo: alimento balanceado, correas, collares, juguetes, etc.</p>
+            <h2>¿Qué marcas trabajamos?</h2>
+            <p>Trabajamos con las marcas más reconocidas del mercado, como por ejemplo: Royal Canin, Eukanuba, Pro Plan, etc.</p>
+            <h2>¿Qué métodos de pago aceptamos?</h2>
+            <p>Aceptamos pagos en efectivo, tarjeta de débito y tarje   ta de crédito.</p>
+            <h2>¿Qué tarjetas de crédito aceptamos?</h2>
+            <p>Aceptamos tarjetas de crédito Visa, Mastercard, American Express, Cabal, Naranja, Nativa, Diners Club, Argencard, Cencosud, Tarjeta Shopping, Tarjeta Naranja, Tarjeta Nevada, Tarjeta Credial, Tarjeta CMR, Tarjeta Cordobesa, Tarjeta Sol, Tarjeta Tuya, Tarjeta Cuyana, Tarjeta Provencred, Tarjeta Walmart, Tarjeta La Anónima, Tarjeta Carrefour, Tarjeta Cencosud, Tarjeta Cetrogar, Tarjeta Cooperativa Obrera, Tarjeta Credimas, Tarjeta Credipaz, Tarjeta Credishop, Tarjeta Credithos, Tarjeta Credito Argentino, Tarjeta Credito Facil,
+            <p>Si no encontraste tu tarjeta en la lista, no dudes en consultarnos.</p>
+            <h2>¿Qué servicios ofrecemos?</h2>
+            <p>Ofrecemos servicios de atención veterinaria, peluquería canina y felina, y venta de alimentos y accesorios para mascotas.</p>
+        </div>
+    </div>
+</div>
