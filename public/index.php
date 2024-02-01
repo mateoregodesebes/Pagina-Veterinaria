6cbe98b1f613c3f888d2c536f3c1b82a310be7ff
JosepSoc <jose.socolsky@gmail.com>
2024-02-01 18:31:17 -0300

    fixed: error mio de css en un merge anterior

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ session_start();
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/veterinaria.css">
     <link rel="stylesheet" href="css/navbar.css">
-    <link rel="stylesheet" href="css/contact.css">
+
     <script src="https://kit.fontawesome.com/ac9e2dd316.js" crossorigin="anonymous"></script>
     <?php
     if (isset($_SESSION["currentPage"])) {
