bf21f26efc36e22ee95931880d228f981bf83a53
mateoregodesebes <mateoregodesebes123@gmail.com>
2024-02-07 22:07:15 -0300

    feat:fixed the redirection to registration.php

--- src/partials/navbar.php
+++ src/partials/navbar.php
@@ -1,5 +1,8 @@
 <div class="row">
     <nav class="nav navbar-expand d-flex justify-content-between">
+        <?php
+        #ToDo: Make so that the logo change the $_SESSION['currentPage'] to contact.php
+        ?>
         <a class="navbar-brand mx-3" href="index.php">
             <img src="../assets/logo.png" width="50" height="50" alt="Logo">
         </a>
@@ -23,10 +26,9 @@
                 </a>
 
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <?php 
-                    if(isset($_SESSION["user"]))
-                    {
-                    # Maybe, I could remove the echo and use the html directly. Closing the php tag and opening it again before the }
+                    <?php
+                    if (isset($_SESSION["user"])) {
+                        # Maybe, I could remove the echo and use the html directly. Closing the php tag and opening it again before the }
                     ?>
                         <div class="alert alert-success" role="alert">Bienvenido $user_name</div>
                         <a class="dropdown-item" href="#">Perfil</a>
@@ -37,30 +39,37 @@
                     <?php
                     }
                     // ToDo: Make the remember me button work 
-                    else
-                    {
-                        require_once(__DIR__ . '/../scripts/login.php');
+                    else {
+                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                            if (isset($_POST["login"])) {
+                                require_once(__DIR__ . '/../scripts/login.php');
+                            } elseif (isset($_POST["registerButton"])) {
+                                $_SESSION['currentPage'] = '../src/pages/registration/registration.php';
+                                echo '<script>window.location.replace("index.php");</script>';
+                                exit();
+                            }
+                        }
                     ?>
-                    <form class="px-4 py-3">
-                        <div class="form-group">
-                            <label>Mail</label>
-                            <input type="email" class="form-control mt-1" id="exampleDropdownFormEmail1" placeholder="email@ejemplo.com" required>
-                        </div>
-                        <div class="form-group">
-                            <label>Contraseña</label>
-                            <input type="password" class="form-control mt-1" id="exampleDropdownFormPassword1" title="Ingrese una contraseña de al menos 8 caracteres" required>
-                        </div>
-                        <div class="form-check mt-2">
-                            <input type="checkbox" class="form-check-input" id="dropdownCheck">
-                            <label class="form-check-label" for="dropdownCheck">
-                                Recordarme
-                            </label>
-                        </div>
-                        <button type="submit" class="btn btn-primary mx-auto iniciar_sesion" name="login">Iniciar Sesion</button>
-                    </form>
-                    <div class="dropdown-divider"></div>
-                    <button type="submit" class="dropdown-item register_button btn btn-secondary" name="registerButton">No tenes una cuenta? Registrate</button>
-                    <a class="dropdown-item" href="#">Olvidaste tu contraseña?</a>
+                        <form class="px-4 py-3">
+                            <div class="form-group">
+                                <label>Mail</label>
+                                <input type="email" class="form-control mt-1" id="exampleDropdownFormEmail1" placeholder="email@ejemplo.com" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Contraseña</label>
+                                <input type="password" class="form-control mt-1" id="exampleDropdownFormPassword1" title="Ingrese una contraseña de al menos 8 caracteres" required>
+                            </div>
+                            <div class="form-check mt-2">
+                                <input type="checkbox" class="form-check-input" id="dropdownCheck">
+                                <label class="form-check-label" for="dropdownCheck">
+                                    Recordarme
+                                </label>
+                            </div>
+                            <button type="submit" class="btn btn-primary mx-auto iniciar_sesion" name="login">Iniciar Sesion</button>
+                        </form>
+                        <div class="dropdown-divider"></div>
+                        <button type="submit" class="dropdown-item register_button btn btn-secondary" name="registerButton">No tenes una cuenta? Registrate</button>
+                        <a class="dropdown-item" href="#">Olvidaste tu contraseña?</a>
                     <?php
                     }
                     ?>
