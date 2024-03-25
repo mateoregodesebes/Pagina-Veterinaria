bb6321d3f96073acffc3a138ed702260f2b28609
Valen Alesa <alesandronivalentino@gmail.com>
2024-03-24 23:02:56 -0300

    Ahora si, fixed

--- src/pages/homepage/homepage.php
+++ src/pages/homepage/homepage.php
@@ -1,4 +1,4 @@
-<div class="container-fluid">
+
     <div class="row fondo">
         <div class="mb-4"></div>
         <div class="col-md-4">
@@ -18,30 +18,38 @@
     </div>
     <div class="mb-4"></div>
     <div class="row">
-        <div class="col-md-3">
-            <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="https://cdn.download.ams.birds.cornell.edu/api/v1/asset/95481341/1800" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Peluquería</h5>
-            </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="https://static.fundacion-affinity.org/cdn/farfuture/PVbbIC-0M9y4fPbbCsdvAD8bcjjtbFc0NSP3lRwlWcE/mtime:1643275542/sites/default/files/los-10-sonidos-principales-del-perro.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Atención a domicilio</h5>
-            </div>
+
+        <div class="col-md-3 col-12">
+            <div class="card">
+                <img class="card-img-top" src="https://cdn.download.ams.birds.cornell.edu/api/v1/asset/95481341/1800" alt="Card image cap">
+                <div class="card-img-overlay">
+                <h5 class="card-title" style="color: white;">Peluquería</h5>
+                </div>
             </div>
         </div>
-        <div class="col-md-3">
-            <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="https://pbs.twimg.com/amplify_video_thumb/1743686164731637760/img/uXiVAF1NNnZ7uhCv.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Hospitalización</h5>
-            </div>
+
+        <div class="col-md-3 col-12">
+            <div class="card">
+                <img class="card-img-top" src="https://static.fundacion-affinity.org/cdn/farfuture/PVbbIC-0M9y4fPbbCsdvAD8bcjjtbFc0NSP3lRwlWcE/mtime:1643275542/sites/default/files/los-10-sonidos-principales-del-perro.jpg" alt="Card image cap">
+                <div class="card-img-overlay">
+                <h5 class="card-title" style="color: white;">Atencion a domicilio</h5>
+                </div>
             </div>
         </div>
+
+    
+        <div class="col-md-3 col-12">
+            <div class="card">
+                <img class="card-img-top" src="https://pbs.twimg.com/amplify_video_thumb/1743686164731637760/img/uXiVAF1NNnZ7uhCv.jpg" alt="Card image cap">
+                <div class="card-img-overlay">
+                <h5 class="card-title" style="color: white;">Hospitalización</h5>
+                </div>
+            </div>
+        </div>
+
+
+
+        <!--
         <div class="col-md-3">
             <div class="card" style="width: 18rem;">
             <img class="card-img-top" src="https://content.nationalgeographic.com.es/medio/2023/03/17/cenzontle-pajaro_0b18132a_230317114903_800x800.jpg" alt="Card image cap">
@@ -50,6 +58,17 @@
             </div>
             </div>
         </div>
+        -->
+
+        <div class="col-md-3 col-12">
+            <div class="card">
+                <img class="card-img-top" src="https://content.nationalgeographic.com.es/medio/2023/03/17/cenzontle-pajaro_0b18132a_230317114903_800x800.jpg" alt="Card image cap">
+                <div class="card-img-overlay">
+                <h5 class="card-title" style="color: white;">Tienda</h5>
+                </div>
+            </div>
+        </div>
+
+
     </div>
-</div>
     
