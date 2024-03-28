f5af43d2fd7fc401df08fa18148daf477a81640d
mateoregodesebes <mateoregodesebes123@gmail.com>
2024-03-28 19:18:47 -0300

    Fixed: fondo vertical problem, whatsapp icon display

--- src/pages/homepage/homepage.php
+++ src/pages/homepage/homepage.php
@@ -1,55 +1,56 @@
-
-    <div class="row fondo">
-        <div class="mb-4"></div>
-        <div class="col-md-4">
-            <h1>¿Tu mascota necesita ayuda?</h1>
-            <h3>Comunicate por asistencia a domicilio.</h3>
-            <div align="right">
-                <a class="whatsapp-link" href="https://chat.whatsapp.com/HJUCUqL6SON3BkIJs6NlvX"><i class="fa-brands fa-whatsapp fa-4x icono"></i></a>
+<div class="row fondo">
+    <div class="col-md-4 my-5">
+        <h1>¿Tu mascota necesita ayuda?</h1>
+        <h3>Comunícate por asistencia a domicilio.</h3>
+        <div class="row">
+            <div class="col-md-10">
             </div>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
+            <div class="col-md-2 text-center mt-md-0 mt-3">
+                <a class="whatsapp-link d-inline-block" href="https://chat.whatsapp.com/HJUCUqL6SON3BkIJs6NlvX">
+                    <i class="fa-brands fa-whatsapp fa-3x icono"></i>
+                </a>
+            </div>
         </div>
     </div>
-    <div class="mb-4"></div>
-    <div class="row">
+</div>
+<div class="row mb-5 mt-4 g-2">
 
-        <div class="col-md-3 col-12">
+    <div class="col-md-3 col-12 cartas">
+        <button>
             <div class="card">
                 <img class="card-img-top" src="https://cdn.download.ams.birds.cornell.edu/api/v1/asset/95481341/1800" alt="Card image cap">
                 <div class="card-img-overlay">
-                <h5 class="card-title" style="color: white;">Peluquería</h5>
+                    <h5 class="card-title" style="color: white;">Peluquería</h5>
                 </div>
             </div>
-        </div>
+        </button>
+    </div>
 
-        <div class="col-md-3 col-12">
+    <div class="col-md-3 col-12 cartas">
+        <button>
             <div class="card">
                 <img class="card-img-top" src="https://static.fundacion-affinity.org/cdn/farfuture/PVbbIC-0M9y4fPbbCsdvAD8bcjjtbFc0NSP3lRwlWcE/mtime:1643275542/sites/default/files/los-10-sonidos-principales-del-perro.jpg" alt="Card image cap">
                 <div class="card-img-overlay">
-                <h5 class="card-title" style="color: white;">Atencion a domicilio</h5>
+                    <h5 class="card-title" style="color: white;">Atencion a domicilio</h5>
                 </div>
             </div>
-        </div>
+        </button>
+    </div>
 
-    
-        <div class="col-md-3 col-12">
+
+    <div class="col-md-3 col-12 cartas">
+        <button>
             <div class="card">
                 <img class="card-img-top" src="https://pbs.twimg.com/amplify_video_thumb/1743686164731637760/img/uXiVAF1NNnZ7uhCv.jpg" alt="Card image cap">
                 <div class="card-img-overlay">
-                <h5 class="card-title" style="color: white;">Hospitalización</h5>
+                    <h5 class="card-title" style="color: white;">Hospitalización</h5>
                 </div>
             </div>
-        </div>
+        </button>
+    </div>
 
 
-
-        <!--
+    <!--
         <div class="col-md-3">
             <div class="card" style="width: 18rem;">
             <img class="card-img-top" src="https://content.nationalgeographic.com.es/medio/2023/03/17/cenzontle-pajaro_0b18132a_230317114903_800x800.jpg" alt="Card image cap">
@@ -60,15 +61,15 @@
         </div>
         -->
 
-        <div class="col-md-3 col-12">
+    <div class="col-md-3 col-12 cartas">
+        <button>
             <div class="card">
                 <img class="card-img-top" src="https://content.nationalgeographic.com.es/medio/2023/03/17/cenzontle-pajaro_0b18132a_230317114903_800x800.jpg" alt="Card image cap">
                 <div class="card-img-overlay">
-                <h5 class="card-title" style="color: white;">Tienda</h5>
+                    <h5 class="card-title" style="color: white;">Tienda</h5>
                 </div>
             </div>
-        </div>
-
-
+        </button>
     </div>
-    
+
+</div>
