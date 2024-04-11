13e3a03303944a15982f65f2c1305ace56d33a97
Valen Alesa <alesandronivalentino@gmail.com>
2024-04-11 00:44:32 -0300

    homepage commit

--- src/pages/homepage/homepage.php
+++ src/pages/homepage/homepage.php
@@ -7,18 +7,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     if (isset($_POST["PeluqButton"])) {
         $_SESSION['atencion'] = 'Corte de pelo';
-        $_SESSION['currentPage'] = '../src/pages/atencionDom/formAtencion.php';
+        $_SESSION['currentPage'] = '../src/pages/peluqueria/peluqueria.php';
         echo '<script>window.location.replace("index.php");</script>';
         exit();
     }
     if (isset($_POST["AtButton"])) {
         $_SESSION['atencion'] = 'Revisión Médica';
-        $_SESSION['currentPage'] = '../src/pages/atencionDom/formAtencion.php';
+        $_SESSION['currentPage'] = '../src/pages/atencion/atencion.php';
         echo '<script>window.location.replace("index.php");</script>';
         exit();
     }
     if (isset($_POST["HospButton"])) {
-        $_SESSION['currentPage'] = '../src/pages/atencionDom/formAtencion.php';
+        $_SESSION['currentPage'] = '../src/pages/hospitalizacion/hospitalizacion.php';
         echo '<script>window.location.replace("index.php");</script>';
         exit();
     }
@@ -47,7 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <form action="index.php" method="post">
             <button type="submit" name="PeluqButton">
                 <div class="card">
-                    <img class="card-img-top" src="https://cdn.download.ams.birds.cornell.edu/api/v1/asset/95481341/1800" alt="Card image cap">
+                    <img class="card-img-top" src="..\assets\loro-card.png" alt="Card image cap">
                     <div class="card-img-overlay">
                         <h5 class="card-title" style="color: white;">Peluquería</h5>
                     </div>
@@ -60,7 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <form action="index.php" method="post">
             <button type="submit" name="AtButton">
                 <div class="card">
-                    <img class="card-img-top" src="https://static.fundacion-affinity.org/cdn/farfuture/PVbbIC-0M9y4fPbbCsdvAD8bcjjtbFc0NSP3lRwlWcE/mtime:1643275542/sites/default/files/los-10-sonidos-principales-del-perro.jpg" alt="Card image cap">
+                    <img class="card-img-top" src="..\assets\perro-card.png" alt="Card image cap">
                     <div class="card-img-overlay">
                         <h5 class="card-title" style="color: white;">Atencion a domicilio</h5>
                     </div>
@@ -74,7 +74,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <form action="index.php" method="post">
             <button type="submit" name="HospButton">
                 <div class="card">
-                    <img class="card-img-top" src="https://pbs.twimg.com/amplify_video_thumb/1743686164731637760/img/uXiVAF1NNnZ7uhCv.jpg" alt="Card image cap">
+                    <img class="card-img-top" src="..\assets\gato-card.png" alt="Card image cap">
                     <div class="card-img-overlay">
                         <h5 class="card-title" style="color: white;">Hospitalización</h5>
                     </div>
@@ -83,23 +83,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </form>
     </div>
 
-
-    <!--
-        <div class="col-md-3">
-            <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="https://content.nationalgeographic.com.es/medio/2023/03/17/cenzontle-pajaro_0b18132a_230317114903_800x800.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Tienda</h5>
-            </div>
-            </div>
-        </div>
-        -->
-
     <div class="col-md-3 col-12 cartas">
         <form action="index.php" method="post">
             <button type="submit" name="shopButton">
                 <div class="card">
-                    <img class="card-img-top" src="https://content.nationalgeographic.com.es/medio/2023/03/17/cenzontle-pajaro_0b18132a_230317114903_800x800.jpg" alt="Card image cap">
+                    <img class="card-img-top" src="..\assets\pajaro-card.png" alt="Card image cap">
                     <div class="card-img-overlay">
                         <h5 class="card-title" style="color: white;">Tienda</h5>
                     </div>
