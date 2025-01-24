a6e62dd6780ef58c28859a9a4a57722ea3a47f9f
JosephSoc <jose.socolsky@gmail.com>
2025-01-23 22:34:59 -0300

    cannot write properly

--- src/partials/pageHandler.php
+++ src/partials/pageHandler.php
@@ -10,7 +10,7 @@ $in_progress_pages = [
 if (!isset($_SESSION["currentPage"])) {
     //?Aca lo que habria que hacer es que en vez de abmList cuando tengamos el home hecho poner eso
     //?y que cuando se apreta el boton de home se ponga el home como current page
-    require_once("../src/pages/homeoage/homepage.php");
+    require_once("../src/pages/homepage/homepage.php");
 } elseif (isset($_SESSION["currentPage"])) {
     // Si la pagina actual esta en el array de paginas en progreso, se muestra la pagina de en progreso
     if (in_array($_SESSION["currentPage"], $in_progress_pages)) {
