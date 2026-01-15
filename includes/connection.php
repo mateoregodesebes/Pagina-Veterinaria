db686e109f9defb436859ffa417f2273c5ffab25
mateoregodesebes <mateoregodesebes123@gmail.com>
2026-01-15 13:30:53 -0300

    Removed echo from connection.php to allow ajax queries

--- includes/connection.php
+++ includes/connection.php
@@ -17,5 +17,4 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo '<script>console.log("Connected successfully");</script>';
 ?>
