dd9632412c12e93c0b760b85b0b67ba9d419e32d
mateoregodesebes <mateoregodesebes123@gmail.com>
2026-02-22 17:45:50 -0300

    Improved sitemap making it dynamic based by role, removed profile buttons to Assistant

--- src/partials/footer.php
+++ src/partials/footer.php
@@ -1,3 +1,51 @@
+<?php
+
+$sitemap_role = $_SESSION["user_role"] ?? 'cliente';
+
+$sitemap = array(
+    "cliente" => array(
+    "La veterinaria" => array(
+        "Inicio" => "home",
+        "Quienes Somos" => "about-us",
+        "Contacto" => "contact",
+        "Noticias peludas" => "newspaper"
+        ),
+    "Servicios y funciones" => array(
+        "Listado de precios para servicios" => "services",
+        "Pedido de turnos" => "requestAppointment",
+        "Tienda online" => "shop"
+        ),
+    "Usuario" => array(
+        "Registrarse" => "register",
+        "Mi perfil" => "profile"
+        )
+    ),
+    "Veterinario" => array(
+        "Atención domiciliaria" => array(
+            "Inicio" => "home"
+            ),
+        "Usuario" => array(
+            "Mi perfil" => "profile"
+            )
+    ),
+    "Peluquero" => array(
+        "Atención domiciliaria" => array(
+            "Inicio" => "home"
+            ),
+        "Usuario" => array(
+            "Mi perfil" => "profile"
+            )
+    ),
+    'Asistente' => array()
+);
+
+$sitemap_col_length = $sitemap_role != 'Asistente' ? 12 / count($sitemap[$sitemap_role]) : 0; 
+
+$hasSitemap = $sitemap_role === 'Asistente' ? false : true;
+
+?>
+
+
 
 <div class="row">
     <div class="col-6 footerblock">
@@ -27,89 +75,38 @@
             </div>
         </div>
     </div>
+
+    <?php
+    if($hasSitemap){
+    ?>
     <div class="col-12 sitemap">
-        <div class="col-4 sitemap-elem"> 
-            <h3>La veterinaria</h3>
+        <?php
+        foreach($sitemap[$sitemap_role] as $sitemap_elem_title => $sitemap_elem_array)  {
+        ?>
+            <div class="col-<?=$sitemap_col_length?> sitemap-elem"> 
+            <h3><?=$sitemap_elem_title?></h3>
             <ul>
+            <?php
+            foreach($sitemap_elem_array as $sitemap_elem_desc => $sitemap_route){
+                ?>
                 <li>
                     <form action="index.php" method="post">
-                        <button type="submit" name="home"> 
-                            Inicio
+                        <button type="submit" name="<?=$sitemap_route?>"> 
+                            <?=$sitemap_elem_desc?>
                         </button>
-                    </form>
+                    </form> 
                 </li>
-                <li>
-                    <form action="index.php" method="post">
-                        <button type="submit" name="about-us"> 
-                            Quienes Somos
-                        </button>
-                    </form>
-                </li>
-                <li>
-                    <form action="index.php" method="post">
-                        <button type="submit" name="contact"> 
-                            Contacto
-                        </button>
-                    </form>
-                </li>
-                <li>
-                    <form action="index.php" method="post">
-                        <button type="submit" name="newspaper"> 
-                            Noticias peludas
-                        </button>
-                    </form>
-                </li>
-            </ul>
-        </div>
-        <div class="col-4 sitemap-elem"> 
-            <h3>Servicios y funciones</h3>
-            <ul>
-                <li>
-                    <form action="index.php" method="post">
-                        <button type="submit" name="services">
-                            Listado de precios para servicios
-                        </button>
-                    </form>
-                </li>
-                <li>
-                    <form action="index.php" method="post">
-                        <button type="submit" name=".">
-                        <?php
-                        //! Cambiar el name del boton cuando se haga la pagina de turnos
-                        ?>
-                            Pedido de turnos
-                        </button>
-                    </form>
-                </li>
-                <li>
-                    <form action="index.php" method="post">
-                        <button type="submit" name="shop">
-                            Tienda online
-                        </button>
-                    </form>
-                </li>
-            </ul>
-        </div>
-        <div class="col-4 sitemap-elem"> 
-            <h3>Usuario</h3>
-            <ul>
-                <li>
-                    <form action="index.php" method="post">
-                        <button type="submit" name="register">
-                            Registrarse
-                        </button>
-                    </form>
-                </li>
-                <li>
-                    <form action="index.php" method="post">
-                        <button type="submit" name="profile">
-                            Mi perfil
-                        </button>
-                    </form>
-                </li>
-            </ul>
-        </div>
+                <?php
+            }
+            ?>
+            </div>
+        <?php
+        }
+        ?>
     </div>
+    <?php
+    }
+    ?>
     <p class="note">Este sitio es realizado para una prueba práctica de la materia Entornos Gráficos de la Universidad Tecnologica
         Nacional Regional Rosario (UTNFRRO), no representa a ninguna veterinaria ni debe ser tomada como ello.</p>
 </div>
--- src/partials/navbar.php
+++ src/partials/navbar.php
@@ -1,10 +1,10 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST["LogoButton"])) {
+    if (isset($_POST["LogoButton"]) || isset($_POST["home"])) {
 
         if(!isset($_SESSION['user_role']) || $_SESSION['user_role'] === 'cliente') {
             unset($_SESSION['currentPage']);
-        } elseif($_SESSION['user_role'] === 'Veterinario') {
+        } elseif($_SESSION['user_role'] === 'Veterinario' || $_SESSION['user_role'] === 'Peluquero' || $_SESSION['user_role'] === 'Asistente') {
             $_SESSION['currentPage'] = '../src/pages/homepage/homepage.php';
         }
 
@@ -23,6 +23,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     elseif (isset($_POST["viewAppointments"])) {
         $_SESSION['currentPage'] = '../src/pages/viewAppointments/viewAppointments.php';
+        echo '<script>window.location.replace("index.php");</script>';
+        exit();
+    }
+    elseif (isset($_POST["profile"])) {
+        $_SESSION['currentPage'] = '../src/pages/profile/profile.php';
+        echo '<script>window.location.replace("index.php");</script>';
+        exit();
+    }
+    elseif (isset($_POST["requestAppointment"])) {
+        $_SESSION['currentPage'] = '../src/pages/requestAppointment/requestAppointment.php';
+        echo '<script>window.location.replace("index.php");</script>';
+        exit();
+    }
+    elseif (isset($_POST["shop"])) {
+        $_SESSION['currentPage'] = '../src/pages/shop/shop.php';
         echo '<script>window.location.replace("index.php");</script>';
         exit();
     }
@@ -120,18 +135,17 @@ $(document).ready(function(){
                                 session_destroy();
                                 echo '<script>window.location.replace("index.php");</script>';
                                 exit();
-                            } elseif (isset($_POST["profileButton"])) {
-                                $_SESSION['currentPage'] = '../src/pages/profile/profile.php';
-                                echo '<script>window.location.replace("index.php");</script>';
-                                exit();
                             }
                         ?>
                             <div class='alert alert-success' role='alert'>Bienvenido
-                                <?php echo $_SESSION["user_name"] ?>
+                                <?= $_SESSION["user_name"] ?>
                             </div>
+                            <?php
+                            if($_SESSION["user_role"] != 'Asistente') {
+                            ?>
                             <div>
                                 <form method="post">
-                                    <button type="submit" class="dropdown-item" name="profileButton">
+                                    <button type="submit" class="dropdown-item" name="profile">
                                         Perfil
                                     </button>
                                 </form>
@@ -143,6 +157,9 @@ $(document).ready(function(){
                                     </button>
                                 </form>
                             </div>
+                            <?php
+                            }
+                            ?>
                             <div class="dropdown-item">
                                 <form method="post">
                                     <button type="submit" class="dropdown-item register_button" name="logoutButton">
